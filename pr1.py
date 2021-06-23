a8aefca0998820ffaf6ad8a00eb1a6ed9db67213
RaviSolanki27 <ravisolanki9322@gmail.com>
2021-06-23 16:26:53 +0530

    Initial commit

---
+++ pr1.py
@@ -0,0 +1,12 @@
+# 1. Python program to add two numbers
+
+a=float(input("Enter first num :"))
+b=float(input("Enter second num :"))
+print("The sum is ", a+b)
+
+# 2. Write a program to check if a number is Odd or even. Take input from user.
+
+c=int(input("Enter the number : "))
+if (c%2==0):
+    print("The number is even")
+else:  print("The number is not even")
